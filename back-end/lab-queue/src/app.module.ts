6aa1441cd8795b65866d284cbc919f9cfef64a52
Nikolay Shestovskih <shest1999@gmail.com>
2021-04-01 01:03:16 +0300

    #26 add db connection using typeorm

--- back-end/lab-queue/src/app.module.ts
+++ back-end/lab-queue/src/app.module.ts
@@ -7,9 +7,15 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { RequestService } from './request/request.service';
 import { ProfileService } from './profile/profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+      UsersModule,
+      AuthModule,
+      TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController, QueueController],
   providers: [AppService, QueueService, RequestService, ProfileService],
 })
