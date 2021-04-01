9a09d5bb119f368b56b079ab32b23b468c810a5d
Korotkov Andrey <akorotkov1234@yandex.ru>
2021-04-01 16:03:46 +0300

    #39 add tests for back-end

--- back-end/lab-queue/src/queue/queue.controller.spec.ts
+++ back-end/lab-queue/src/queue/queue.controller.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QueueController } from './queue.controller';
+import { QueueService } from './queue.service';
+import { RequestService } from '../request/request.service';
+import { ProfileService } from '../profile/profile.service';
 
 describe('QueueController', () => {
-  let controller: QueueController;
+	let controller: QueueController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [QueueController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [QueueController],
+			providers: [QueueService, RequestService, ProfileService],
+		}).compile();
 
-    controller = module.get<QueueController>(QueueController);
-  });
+		controller = module.get<QueueController>(QueueController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
 });
