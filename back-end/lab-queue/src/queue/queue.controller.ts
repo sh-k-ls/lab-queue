2627115e8922a53c33ccfb277db3ead58629e104
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-04-05 15:57:37 +0300

    #38 change tabs to spaces

--- back-end/lab-queue/src/queue/queue.controller.ts
+++ back-end/lab-queue/src/queue/queue.controller.ts
@@ -1,14 +1,14 @@
 import {
-	Body,
-	Controller,
-	Get,
-	HttpCode,
-	HttpStatus,
-	Param,
-	Patch,
-	Post,
-	Req,
-	UseGuards,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueDto } from '../shared/classes/queue.dto';
@@ -22,100 +22,100 @@ import { UserDto } from '../shared/classes/user.dto';
 
 @Controller('api/v1/queue')
 export class QueueController {
-	constructor(
-		private readonly queue: QueueService,
-		private readonly request: RequestService,
-		private readonly profile: ProfileService,
-	) {}
+  constructor(
+    private readonly queue: QueueService,
+    private readonly request: RequestService,
+    private readonly profile: ProfileService,
+  ) {}
 
-	@UseGuards(JwtAuthGuard)
-	@Post()
-	@HttpCode(HttpStatus.CREATED)
-	createQueue(@Body() queue: QueueDto): number {
-		return this.queue.pushQueue(queue);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createQueue(@Body() queue: QueueDto): number {
+    return this.queue.pushQueue(queue);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get('available')
-	getAllQueuesAvailable(@Req() req: Request): QueueDto[] {
-		return this.queue.getByUserAvailableId(<UserDto>req.user);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get('available')
+  getAllQueuesAvailable(@Req() req: Request): QueueDto[] {
+    return this.queue.getByUserAvailableId(<UserDto>req.user);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get('creator')
-	getAllQueuesCreator(@Req() req: Request): QueueDto[] {
-		return this.queue.getByUserCreatorId(<UserDto>req.user);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get('creator')
+  getAllQueuesCreator(@Req() req: Request): QueueDto[] {
+    return this.queue.getByUserCreatorId(<UserDto>req.user);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get('signed')
-	getAllQueuesSigned(@Req() req: Request): QueueDto[] {
-		return this.queue.getByUserSignedId(<UserDto>req.user);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get('signed')
+  getAllQueuesSigned(@Req() req: Request): QueueDto[] {
+    return this.queue.getByUserSignedId(<UserDto>req.user);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get(':id')
-	getQueueById(@Param('id') idQueue: string): QueueDto {
-		return this.queue.getByQueueId(+idQueue);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  getQueueById(@Param('id') idQueue: string): QueueDto {
+    return this.queue.getByQueueId(+idQueue);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get(':id/request')
-	getRequestsByQueueId(@Param('id') idQueue: string): RequestDto[] {
-		return this.request.getByQueueId(+idQueue);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get(':id/request')
+  getRequestsByQueueId(@Param('id') idQueue: string): RequestDto[] {
+    return this.request.getByQueueId(+idQueue);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Post(':id/request')
-	@HttpCode(HttpStatus.CREATED)
-	addRequestsByQueueId(
-		@Param('id') idQueue: string,
-		@Body() queueReq: RequestDto,
-		@Req() req: Request,
-	): number {
-		queueReq.userId = (req.user as UserDto).id;
-		return this.request.pushRequest(queueReq);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/request')
+  @HttpCode(HttpStatus.CREATED)
+  addRequestsByQueueId(
+    @Param('id') idQueue: string,
+    @Body() queueReq: RequestDto,
+    @Req() req: Request,
+  ): number {
+    queueReq.userId = (req.user as UserDto).id;
+    return this.request.pushRequest(queueReq);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Get(':id/request/profile')
-	getProfilesByQueueId(@Param('id') idQueue: string): ProfileDto[] {
-		return this.profile.getProfilesByQueueId(+idQueue);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Get(':id/request/profile')
+  getProfilesByQueueId(@Param('id') idQueue: string): ProfileDto[] {
+    return this.profile.getProfilesByQueueId(+idQueue);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Patch(':id/request')
-	editRequestByQueueId(
-		@Param('id') idQueue: string,
-		@Body() queueReq: RequestDto,
-		@Req() req: Request,
-	): RequestDto {
-		return this.request.changeSigned((req.user as UserDto).id, +idQueue);
-	}
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id/request')
+  editRequestByQueueId(
+    @Param('id') idQueue: string,
+    @Body() queueReq: RequestDto,
+    @Req() req: Request,
+  ): RequestDto {
+    return this.request.changeSigned((req.user as UserDto).id, +idQueue);
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Patch(':id')
-	editQueueById(@Param('id') id: string, @Body() queue: QueueDto): QueueDto {
-		return queue;
-	}
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  editQueueById(@Param('id') id: string, @Body() queue: QueueDto): QueueDto {
+    return queue;
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Post(':id/signIn')
-	signInQueue(@Param('id') queueId: string, @Req() req: Request): number {
-		return this.request.pushRequest({
-			queueId: +queueId,
-			userId: (req.user as UserDto).id,
-			isSigned: true,
-		});
-	}
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/signIn')
+  signInQueue(@Param('id') queueId: string, @Req() req: Request): number {
+    return this.request.pushRequest({
+      queueId: +queueId,
+      userId: (req.user as UserDto).id,
+      isSigned: true,
+    });
+  }
 
-	@UseGuards(JwtAuthGuard)
-	@Patch(':id/signOut')
-	sighOutQueue(@Param('id') queueId: string, @Req() req: Request): number {
-		return this.request.delRequest({
-			queueId: +queueId,
-			userId: (req.user as UserDto).id,
-			isSigned: true,
-		});
-	}
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id/signOut')
+  sighOutQueue(@Param('id') queueId: string, @Req() req: Request): number {
+    return this.request.delRequest({
+      queueId: +queueId,
+      userId: (req.user as UserDto).id,
+      isSigned: true,
+    });
+  }
 }
