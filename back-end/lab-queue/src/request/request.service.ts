4b02b291b0bdf8e6dfde28473fc0fd02b304e073
Nikolay Shestovskih <shest1999@gmail.com>
2021-04-16 23:48:18 +0300

    #69 refactor request service

--- back-end/lab-queue/src/request/request.service.ts
+++ back-end/lab-queue/src/request/request.service.ts
@@ -31,21 +31,14 @@ export class RequestService {
 	}
 
 	public isSigned(userId: number): boolean {
-    return (this.requests.find(elem => elem.userId === userId) !== undefined);
+    return !!(this.requests.find(elem => elem.userId === userId));
   }
 
   public getByUserIdQueueId(userId: number, queueId: number): RequestDto {
-    let resRequest: RequestDto;
-    for (const request of this.requests) {
-      if (
-        request.userId === userId &&
-        request.queueId === queueId &&
-        request.isSigned === true
-      ) {
-        resRequest = request;
-      }
-    }
-    return resRequest;
+    return this.requests.find(req =>
+      req.userId === userId &&
+      req.queueId === queueId &&
+      req.isSigned === true);
   }
 
 	public changeSigned(userId: number, queueId: number): RequestDto {
