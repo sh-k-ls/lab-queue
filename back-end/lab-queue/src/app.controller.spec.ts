0c08f460c5f7cc695e70bd439f529b3bb32f9642
Olga Kondrashova <ol.kondraschowa@gmail.com>
2021-04-22 18:14:48 +0300

    #54 add AppController API documentation

--- back-end/lab-queue/src/app.controller.spec.ts
+++ back-end/lab-queue/src/app.controller.spec.ts
@@ -3,19 +3,19 @@ import { AppController } from './app.controller';
 import { AppModule } from './app.module';
 
 describe('AppController', () => {
-	let appController: AppController;
+  let appController: AppController;
 
-	beforeEach(async () => {
-		const app: TestingModule = await Test.createTestingModule({
-			imports: [AppModule],
-		}).compile();
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-		appController = app.get<AppController>(AppController);
-	});
+    appController = app.get<AppController>(AppController);
+  });
 
-	describe('root', () => {
-		it('should return "Hello World!"', () => {
-			expect(appController.getHello()).toBe('Server works...');
-		});
-	});
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Server works...');
+    });
+  });
 });
--- back-end/lab-queue/src/app.controller.ts
+++ back-end/lab-queue/src/app.controller.ts
@@ -1,13 +1,19 @@
-import {Controller, Get, Post, UseGuards, Req, Param} from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Req, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/guard/local-auth.guard';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Request } from 'express';
 import { UserDto } from './shared/front-back-end/user.dto';
-import {ProfileDto} from './shared/front-back-end/profile.dto';
-import {ProfileService} from './profile/profile.service';
+import { ProfileDto } from './shared/front-back-end/profile.dto';
+import { ProfileService } from './profile/profile.service';
+import { JwtDto } from './shared/front-back-end/jwt.dto';
 
+/**
+ * Класс AppController
+ *
+ * Предоставляет API
+ */
 @Controller()
 export class AppController {
   constructor(
@@ -16,17 +22,36 @@ export class AppController {
     private readonly profile: ProfileService,
   ) {}
 
+  /**
+   * Отправляет сообщение о работе сервера, если он запущен
+   *
+   * Сообщение 'Server works...'
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  /**
+   * POST-запрос для входа в аккаунт
+   *
+   *  @param {Request} req Запрос
+   *  @returns {string} JSONWebToken
+   *  @example {jwt: ukwysx726x2uyxuj2xzywjuxyey}
+   */
   @UseGuards(LocalAuthGuard)
   @Post('api/auth/login')
-  async login(@Req() req: Request) {
+  async login(@Req() req: Request): Promise<JwtDto> {
     return this.authService.login(<UserDto>req.user);
   }
 
+  /**
+   * GET-запрос для получения информации об авторизованном пользователе
+   *
+   *  @param {Request} req Запрос
+   *  @returns {string} JSONWebToken
+   *  @example {jwt: ukwysx726x2uyxuj2xzywjuxyey}
+   */
   @UseGuards(JwtAuthGuard)
   @Get('api/v1/profile')
   getProfile(@Req() req: Request) {
