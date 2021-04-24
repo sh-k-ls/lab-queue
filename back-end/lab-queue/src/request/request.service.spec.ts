32c98226595f0cc2f3c30389f2b8b8fdfe2de454
Korotkov Andrey <akorotkov1234@yandex.ru>
2021-04-24 23:39:13 +0300

    #92 fixed tests for ci, which fail in #54

--- back-end/lab-queue/src/request/request.service.spec.ts
+++ back-end/lab-queue/src/request/request.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RequestService } from './request.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
+import { RequestEntity } from '../database.entities/request.entity';
+import { UserEntity } from '../database.entities/user.entity';
+import { QueueEntity } from '../database.entities/queue.entity';
 
 describe('RequestService', () => {
   let service: RequestService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RequestService],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'queue',
+          password: 'queue1337',
+          database: 'bmstu_queue',
+          entities: [
+            __dirname + '../../../src/database.entities/*.entity{.ts,.js}',
+          ],
+          migrations: ['migration/*{.ts,.js}'],
+          cli: {
+            migrationsDir: 'migration',
+          },
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([RequestEntity, UserEntity, QueueEntity]),
+      ],
+      providers: [RequestService, UsersService],
     }).compile();
 
     service = module.get<RequestService>(RequestService);
--- back-end/lab-queue/src/users/users.service.spec.ts
+++ back-end/lab-queue/src/users/users.service.spec.ts
@@ -1,11 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../database.entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'queue',
+          password: 'queue1337',
+          database: 'bmstu_queue',
+          entities: [
+            __dirname + '../../../src/database.entities/*.entity{.ts,.js}',
+          ],
+          migrations: ['migration/*{.ts,.js}'],
+          cli: {
+            migrationsDir: 'migration',
+          },
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([UserEntity]),
+      ],
       providers: [UsersService],
     }).compile();
 
@@ -14,5 +36,9 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await module.close();
   });
 });
